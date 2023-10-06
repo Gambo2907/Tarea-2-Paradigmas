e28ded54ec90249688fe6534c15a7bf2696935b7
Gambo2907 <sgamo2220@gmail.com>
2023-10-05 23:27:49 -0600

    codigo medio comentado

--- src/Interfaz.pl
+++ src/Interfaz.pl
@@ -1,9 +1,6 @@
 :-consult(sintaxis).
 :-consult(grafo).
-%TENNGO QUE HACER MAS PRUEBAS CON LA GRAMATICA
-%PORQUE DA ALGUNOS ERRORES CON ALGUNAS ORACIONES
-%DE ENTRADA ACEPTA "en turrialba" o 'estoy en turrialba'
-%voy para "destino"
+
 
 
 %Funcion miembro
@@ -12,47 +9,55 @@ miembro(Elem,[Elem|_]).
 
 
 
-%Para buscar el lugar en una lista de palabras
+%Busca el lugar en una lista de palabras
 buscar_lugar(Input,Destino):- miembro(Destino,Input), lugar([Destino],[]),!.
+
+%Busca el establecimiento en una lista de palabras
 buscar_establecimiento(Input,Establecimiento):- miembro(Establecimiento,Input), establecimiento([Establecimiento],[]),!.
+
+%Busca el nombre del establecimiento en una lista de palabras
 buscar_nombre_establecimiento(Input,NombreEstablecimiento):-miembro(NombreEstablecimiento,Input),nombre_establecimiento([NombreEstablecimiento],[]),!.
 
+%Busca si la palabra es negacion
 buscar_negacion(Input,Negacion):-miembro(Negacion,Input),negacion([Negacion],[]),!.
-%ESTO NO SE ESTA USANDO
-obtener_lugar(L0, L) :-
-    read_line_to_string(user_input, Oracion),
-    dividir_palabras(Oracion,L2),
-    oracion(Oracion,L).
 
+%Mensaje de error
 mensaje_error:- write("No te he entendido o el lugar que ingreso no existe").
 
 % Predicado para dividir una cadena en palabras
 dividir_palabras(Cadena, Palabras) :-
     atomic_list_concat(Palabras, ' ', Cadena).
 
+%Predicado de suma
 suma(X, Y, Z) :- Z is X + Y.
 
-
+%Predicado que quita el primer elemento de una lista
 quitar_primero([_|Resto], Resto).
 
+%Predicado que une listas
 unir(Lista1, Lista2, Resultado) :-
     append(Lista1, Lista2, Resultado).
 
+% Predicado que busca la ruta mas corta, en este se busca sin un lugar
+% intermedio
 buscar(X,Y,0,KM,P,KM2, P2,KM3,P3,P4) :-
     findminpath(X,Y,Km,P),
-    format('La ruta más corta es la siguiente. KM: ~w Ruta mas corta: ~w~n', [Km,P]).
+    format('La ruta más corta es la siguiente.'),nl,
+    format('Ruta mas corta: ~w~n ~w km', [P,Km]),nl.
 
-
-
+% El o logico del predicado buscar, en este caso busca la ruta mas corta
+% con un lugar intermedio
 buscar(X,Y,Z,KM,P,KM2, P2,KM3,P3,P4) :-
     findminpath(X,Z,Km,P),
     findminpath(Z,Y,Km2,P2),
     suma(Km,Km2,Km3),
     quitar_primero(P2,P4),
     unir(P,P4,P3),
-    format('La ruta más corta es la siguiente. KM: ~w Ruta mas corta: ~w~n', [Km3,P3]).
+    format('La ruta más corta es la siguiente.'),nl,
+    format('Ruta mas corta: ~w~n ~w km', [P3,Km3]),nl.
 
-
+% Predicado que consulta al usuario el lugar de inicio para encontrar la
+% ruta mas corta
 consultar_inicio(Inicio):-
 
     write("Wazelog: Por favor indique donde se encuentra"), nl,
@@ -61,60 +66,66 @@ consultar_inicio(Inicio):-
     oracion(ListaPalabras, _),
     buscar_lugar(ListaPalabras, Inicio), !.
 
-consultar_inicio(Inicio):- mensaje_error, nl, consultar_inicio(Inicio).
+consultar_inicio(Inicio):- mensaje_error, nl, consultar_inicio(Inicio). %Si el usuario escribe algo incorrecto tira mensaje de error
 
+% Predicado que consulta al usuario el lugar de destino para encontrar
+% la ruta mas corta
 consultar_destino(Destino):-
-    write("Wazelog: ¡Muy bien!"), nl,
     write("Wazelog: ¿Cual es su destino?: "), nl,
     read_line_to_string(user_input,Oracion2),
     dividir_palabras(Oracion2, ListaPalabras2),
     oracion(ListaPalabras2, L2),
     buscar_lugar(ListaPalabras2, Destino),!.
 
-consultar_destino(Destino):- mensaje_error, nl, consultar_destino(Destino).
+consultar_destino(Destino):- mensaje_error, nl, consultar_destino(Destino). %Si el usuario escribe algo incorrecto tira mensaje de error
 
+% Predicado que consulta al usuario todo sobre la ruta intermedia para
+% encontrar la ruta mas corta
 busqueda_intermedio_positiva(Inicio,Destino,Oracion,Z,Int):-
-    write("Wazelog: ¿Cual "), write(Z),write("?: "),nl,
-    read_line_to_string(user_input,Oracion4),
-    dividir_palabras(Oracion4, ListaPalabras4),
-    oracion(ListaPalabras4, L4),
-    buscar_nombre_establecimiento(ListaPalabras4,NomEst),
-    write(" Wazelog: ¿Donde se encuentra "),write(NomEst),write("?: "),nl,
-    read_line_to_string(user_input,Oracion5),
-    dividir_palabras(Oracion5, ListaPalabras5),
-    oracion(ListaPalabras5, _),
-    buscar_lugar(ListaPalabras5, Int),
-    buscar(Inicio,Destino,Int,_,_,_,_,_,_,_).
+    write("Wazelog: ¿Cual "), write(Z),write("?: "),nl, %pregunta al usuario sobre el nombre del establecimiento que quiere visitar
+    read_line_to_string(user_input,Oracion4), %obtiene la respuesta del usuario y la convierte en string
+    dividir_palabras(Oracion4, ListaPalabras4), %divide las palabras
+    oracion(ListaPalabras4, L4), % obtiene la oracion dividida
+    buscar_nombre_establecimiento(ListaPalabras4,NomEst),!, %busca el nombre del establecimiento y cuando lo encuentra hace el corte para que no busque mas
+    write(" Wazelog: ¿Donde se encuentra "),write(NomEst),write("?: "),nl, %pregunta al usuario donde se encuentra el establecimiento
+    read_line_to_string(user_input,Oracion5),  %obtiene la respuesta del usuario y la convierte en string
+    dividir_palabras(Oracion5, ListaPalabras5), %divide las palabras
+    oracion(ListaPalabras5, _),  % obtiene la oracion dividida
+    buscar_lugar(ListaPalabras5, Int),!, %busca el lugar en donde esta el establecimiento y cuando lo encuentra hace el corte para que no busque mas
+    buscar(Inicio,Destino,Int,_,_,_,_,_,_,_). %busca la ruta mas corta con un intermedio
 
-busqueda_intermedio_positiva(Inicio,Destino,Oracion,Z,Int):- mensaje_error, nl, busqueda_intermedio_positiva(Inicio,Destino,Oracion,Z,Int).
+busqueda_intermedio_positiva(Inicio,Destino,Oracion,Z,Int):- mensaje_error, nl, busqueda_intermedio_positiva(Inicio,Destino,Oracion,Z,Int). %Si el usuario escribe algo incorrecto tira mensaje de error
 
 
-
+%predicado auxiliar para que busque si hay intermedio
 busqueda_intermedio_aux(Inicio,Destino,Oracion,Z):-
-    dividir_palabras(Oracion, ListaPalabras),
-    oracion(ListaPalabras, L3),
-    buscar_establecimiento(ListaPalabras,Z),!,
-    busqueda_intermedio_positiva(Inicio,Destino,Oracion,Z,_).
+    dividir_palabras(Oracion, ListaPalabras), %divide las palabras
+    oracion(ListaPalabras, L3), % obtiene la oracion dividida
+    buscar_establecimiento(ListaPalabras,Z),!, %busca el establecimiento y cuando lo encuentra hace el corte para que no busque mas
+    busqueda_intermedio_positiva(Inicio,Destino,Oracion,Z,_). %llama al predicado de la busqueda positiva
 
+%si no, la busqueda es negativa y llama a este predicado auxiliar
 busqueda_intermedio_aux(Inicio,Destino,Oracion,Z):-
-    dividir_palabras(Oracion, ListaPalabras),
-    oracion(ListaPalabras, L3),
-    buscar_negacion(ListaPalabras,Z),!,
-    buscar(Inicio,Destino,0,_,_,_,_,_,_,_).
+    dividir_palabras(Oracion, ListaPalabras),%divide las palabras
+    oracion(ListaPalabras, L3),% obtiene la oracion dividida
+    buscar_negacion(ListaPalabras,Z),!, %busca si hay alguna negacion y cuando lo encuentra hace el corte para que no busque mas
+    buscar(Inicio,Destino,0,_,_,_,_,_,_,_). %busca la ruta mas corta sin un intermedio
 
+%predicado que busca un intermedio para encontrar la ruta mas corta
 busqueda_intermedio(Inicio,Destino,Oracion,Z):-
-    write('¿Tiene algun destino intermedio?: '),nl,
-    read_line_to_string(user_input,Oracion),
-    busqueda_intermedio_aux(Inicio,Destino,Oracion,Z).
+    write('¿Tiene algun destino intermedio?: '),nl, %pregunta al usuario si tiene un destino intermedio
+    read_line_to_string(user_input,Oracion), %obtiene lo escrito por el usuario y lo convierte en string
+    busqueda_intermedio_aux(Inicio,Destino,Oracion,Z). %llama al predicado auxiliar
 
 
-busqueda_intermedio(Inicio,Destino,Oracion,Z):- mensaje_error, nl, busqueda_intermedio(Inicio,Destino,Oracion,Z).
+busqueda_intermedio(Inicio,Destino,Oracion,Z):- mensaje_error, nl, busqueda_intermedio(Inicio,Destino,Oracion,Z). %Si el usuario escribe algo incorrecto tira mensaje de error
 
-% Pruebas de interfaz
-testeo :-
-    write('Bienvenido a Wazelog la mejor logica para llegar a su destino'),
+% Predicado del SE
+sistemaexperto :-
+    write('Bienvenido a Wazelog la mejor logica para llegar a su destino'), %saludo de inicio
     nl,
-    consultar_inicio(Inicio),%PASO 1. OBTENER EL LUGAR DE INICIO/ORIGEN
-    consultar_destino(Destino),
-    busqueda_intermedio(Inicio,Destino,_,_),
-    format('Gracias por utilizar Wazelog').
+    consultar_inicio(Inicio),%PASO 1. Obtener el lugar de inicio
+    write("Wazelog: ¡Muy bien!"), nl,
+    consultar_destino(Destino), %PASO 2. Obtener el lugar de Destino
+    busqueda_intermedio(Inicio,Destino,_,_), %PASO 3. Obtener si hay ruta intermedia o no
+    format('Gracias por utilizar Wazelog'). %saludo final
--- src/grafo.pl
+++ src/grafo.pl
@@ -47,28 +47,37 @@ arista(cachi,orosi,12).
 arista(orosi,paraiso,8).
 arista(orosi,cachi,12).
 
-findapath(X, Y, W, [X,Y], _) :- arista(X, Y, W). % findapath between X and Y has weight W  if there is an edge between X and Y of weight W
-findapath(X, Y, W, [X|P], V) :-                % else findapath between X and Y
-	\+ member(X, V),                       % is true
-	arista(X, Z, W1),                        % if we can findapath between X and Z with weight W1
-	findapath(Z, Y, W2, P, [X|V]),         % and there is findapat between Z and Y of weight W2
-	W is W1 + W2.                          % where W is W1 + W2
+findapath(X, Y, W, [X,Y], _) :- arista(X, Y, W). % Encuentra una ruta entre X y Y con un peso W, si hay una arista entre X y Y con peso W
+findapath(X, Y, W, [X|P], V) :-                % si no encuentre una ruta entre X y Y
+	\+ member(X, V),                       % es verdadero
+	arista(X, Z, W1),		       % si se encuentra una ruta entre X y Z con un peso W1
+	findapath(Z, Y, W2, P, [X|V]),         % y si encuentra una ruta entre Z y Y con un peso W2
+	W is W1 + W2.                          % donde W es W1 + W2
 
-:-dynamic(solution/2).
-findminpath(Ini, Fin, W, P) :-
-	\+ solution(_, _),
-	findapath(Ini, Fin, W1, P1, []),
-	assertz(solution(W1, P1)),
-	!, findminpath(Ini,Fin,W,P).
+:-dynamic(solution/2). %Esto declara que la relación solution/2 es dinamica, lo que significa que se pueden agregar y eliminar hechos de esta relacion en tiempo de ejecucion.
 
-findminpath(Ini, Fin, _, _) :-
-	findapath(Ini, Fin, W1, P1, []),
-	solution(W2, P2),
-	W1 < W2,
-	retract(solution(W2, P2)),
-	asserta(solution(W1, P1)), fail.
+findminpath(Ini, Fin, W, P) :- % Esta regla se encarga de encontrar el camino mas corto entre los nodos Ini y Fin y devuelve el peso W y el camino P
 
-findminpath(_, _, W, P) :- solution(W,P), retract(solution(W,P)).
+	\+ solution(_, _), % Verifica que no haya ninguna solucion almacenada en la base de datos de soluciones
+
+	findapath(Ini, Fin, W1, P1, []), %Llama a la regla findapath para encontrar un camino entre Ini y Fin con un peso W1 y un camino P1. La lista vacía [] se pasa como lista de nodos visitados inicial
+
+	assertz(solution(W1, P1)), % Almacena esta solucion en la base de datos de soluciones
+
+	!, findminpath(Ini,Fin,W,P). % Utiliza el corte para evitar la exploracion de soluciones adicionales y llama nuevamente a findminpath para verificar si hay soluciones mas cortas
+
+findminpath(Ini, Fin, _, _) :- % Esta regla se utiliza para comparar las soluciones encontradas con las soluciones almacenadas previamente
+	findapath(Ini, Fin, W1, P1, []), %Intenta encontrar un nuevo camino entre Ini y Fin con peso W1 y lo almacena en P1
+
+	solution(W2, P2), %Obtiene una solucion previamente almacenada con peso W2 y camino P2
+
+	W1 < W2, %Compara los pesos
+
+	retract(solution(W2, P2)), %Retira la solucion anterior
+
+	asserta(solution(W1, P1)), fail. %Forza el fracaso de esta regla para que Prolog busque otras soluciones
+
+findminpath(_, _, W, P) :- solution(W,P), retract(solution(W,P)). %Esta regla se utiliza para recuperar la solución final y eliminarla de la base de datos de soluciones una vez que se ha encontrado el camino más corto
 
 
 
